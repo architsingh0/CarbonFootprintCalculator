9c711d9e75848c4235d3049711cf15b2a02dd1f5
Archit <5ingh4rch13@gmail.com>
2024-04-22 17:05:37 -0700

    Initial Commit

---
+++ CarbonFootprintCalculator/CarbonFootprintCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarbonFootprintCalculatorApp.swift
+//  CarbonFootprintCalculator
+//
+//  Created by Archit Singh on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CarbonFootprintCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
